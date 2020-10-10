e44b93e210819baa619407a2b5a1bb243fc1544c
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2020-10-11 02:18:33 +0700

    add events

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -6,13 +6,18 @@ class ProductItem extends Component {
   }
 
   render() {
-    const { productName, unitPrice, thumbnail } = this.props;
+    const { productName, unitPrice, thumbnail } = this.props.product;
     return (
       <div className="col-md-3 col-sm-6">
-        <img className="img-fluid img-thumbnail" src={thumbnail} />
+        <img className="img-fluid img-thumbnail" src={thumbnail} alt="" />
         <h5 className="mt-2">{productName}</h5>
         <p className="title text-right">{unitPrice}</p>
-        <button className="btn btn-block btn-secondary title">ซื้อ</button>
+        <button
+          className="btn btn-block btn-secondary title"
+          onClick={() => this.props.onAddOrder(this.props.product)}
+        >
+          ซื้อ
+        </button>
         <hr />
       </div>
     );
